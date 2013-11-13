51336c2413b38d3eed1c90817b952c72fde91a05
aa46 <aa46@st-andrews.ac.uk>
2013-11-13 14:53:29 +0000

    removed hidden and tmp files

--- TourneyJourney/tmp/Monster.pde
+++
@@ -1,5 +0,0 @@
-class Monster {
-  int x, y;
-  int w, h;
-  int hp, max_hp;
-}
--- TourneyJourney/tmp/PlayerState.java
+++
@@ -1,4 +0,0 @@
-// list of possible states the player can be in
-public enum PlayerState {
-  SPAWNING, STANDING, MOVING, ATTACKING, HURT, DEAD, NUM_STATES;
-}
